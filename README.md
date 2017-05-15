4bef114d0a55490f59ef3d57c8fc824658163b43
Yvo van Doorn <yvo.vandoorn@gmail.com>
2017-05-15 09:45:12 +0200

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# smb1server_controlremediate
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'smb1server_controlremediate'
+maintainer 'The Authors'
+maintainer_email 'yvo.vandoorn@gmail.com'
+license 'apachev2'
+description 'Installs/Configures smb1server_controlremediate'
+long_description 'Installs/Configures smb1server_controlremediate'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/smb1server_controlremediate/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/smb1server_controlremediate' if respond_to?(:source_url)
